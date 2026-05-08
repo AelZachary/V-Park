2f5470d04e9b48a0ab8b2cd706bb1af4a079e8cb
Amirah Nathania <thaniafahreza@gmail.com>
2026-05-08 21:07:32 +0800

    Frontend_PerbaikanNavbarDanTabs

---
+++ frontend/app/user/_layout.tsx
@@ -0,0 +1,44 @@
+import BottomNavbar from '@/components/navigation/BottomNavbar';
+import { Stack, usePathname } from 'expo-router';
+import React from 'react';
+import { View } from 'react-native';
+
+export default function UserLayout() {
+
+  const pathname = usePathname();
+
+  /* HALAMAN YANG TIDAK PAKAI NAVBAR */
+  const hideNavbarPage = [
+    '/user/KonfirmasiSelesaiParkir',
+  ];
+
+  /* CEK APAKAH NAVBAR DISSEMBUNYIKAN */
+  const shouldHideNavbar =
+    hideNavbarPage.includes(pathname);
+
+  const getActiveTab = () => {
+
+    if (pathname.includes('profile')) {
+      return 'profile';
+    }
+
+    if (pathname.includes('activity')) {
+      return 'activity';
+    }
+
+    return 'home';
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+
+      <Stack screenOptions={{ headerShown: false }} />
+
+      {/* NAVBAR */}
+      {!shouldHideNavbar && (
+        <BottomNavbar active={getActiveTab()} />
+      )}
+
+    </View>
+  );
+}
