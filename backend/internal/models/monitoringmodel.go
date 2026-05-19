e2c8e499ef82fe60f6bcecee9a5fd76c34a8fdb7
Aristo Benedict Iskandar <iskandararisto@gmail.com>
2026-05-19 08:01:55 +0800

    chore: add another controller and routes for monitoring petugas

--- backend/internal/models/monitoringmodel.go
+++ backend/internal/models/monitoringmodel.go
@@ -8,5 +8,5 @@ type Monitoring struct {
 	IDMonitoring    uint      `gorm:"primaryKey;not null;autoIncrement"`
 	IDPetugas       uint      `gorm:"not null;uniqueIndex:idx_monitoring_petugas_tempat"`
 	IDTempatParkir  uint      `gorm:"not null;uniqueIndex:idx_monitoring_petugas_tempat"`
-	WaktuMonitoring time.Time `gorm:"type:varchar(50); not null"`
+	WaktuMonitoring time.Time `gorm:"not null"`
 }
