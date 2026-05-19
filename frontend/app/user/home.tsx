e9056563f5b02fac00097d8761e7a1bbe2e0160e
Aelzachary <aelzachary@gmail.com>
2026-05-19 11:32:39 +0800

    processing rawr

--- frontend/app/user/home.tsx
+++ frontend/app/user/home.tsx
@@ -62,7 +62,7 @@ export default function HomeScreen() {
             style={styles.card}
             onPress={() => {
               if (item.name === 'Ground Floor') {
-                router.push('/user/payment');
+                router.push('/user/selectParkingSpot');
               }
             }}
           >
